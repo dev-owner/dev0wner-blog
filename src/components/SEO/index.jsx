c31fd4002eb497bea2267db1050dc16b21f130bc
dev-owner <yureka1112@gmail.com>
2023-01-24 23:15:45 +0900

    remove old naver seo tag

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -12,10 +12,6 @@ const SEO = ({ title, description, url }) => {
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
       <meta name="google-site-verification" content="8igxFYKhZpi1GSVsxyH9_4OqWrGG-QkNrTYUJ-Zs-UU" />
-      <meta
-        name="naver-site-verification"
-        content="c8ad085623ce80efef1d7f4ec91c62354442c9c2"
-      />
       <meta name="naver-site-verification" content="0c3f41db9a1cd632a5f18d0d39f750d2c73c8ef6" />
     </Helmet>
   )
