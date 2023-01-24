fcbde771f2cdd678820c2f542260869526a80cb2
dev-owner <yureka1112@gmail.com>
2023-01-24 23:13:16 +0900

    add seo tags

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -11,6 +11,12 @@ const SEO = ({ title, description, url }) => {
       <meta property="og:image" content={`${siteUrl}/og-image.png`} />
       {description && <meta name="description" content={description} />}
       {description && <meta property="og:description" content={description} />}
+      <meta name="google-site-verification" content="8igxFYKhZpi1GSVsxyH9_4OqWrGG-QkNrTYUJ-Zs-UU" />
+      <meta
+        name="naver-site-verification"
+        content="c8ad085623ce80efef1d7f4ec91c62354442c9c2"
+      />
+      <meta name="naver-site-verification" content="0c3f41db9a1cd632a5f18d0d39f750d2c73c8ef6" />
     </Helmet>
   )
 }
